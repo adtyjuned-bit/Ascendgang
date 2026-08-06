bb0d99a678dcf8b9cf9b57074c6e8b5fa77d14b5
adtyjuned-bit <adtyjuned@gmail.com>
2026-08-07 05:46:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -178,7 +178,7 @@ window.onclick = function(event) {
       {
         name: "NAIN VANCE",
         role: "Owner / Head Admin",
-        avatar: "gambar/staff1.png", // Gantilah sesuai lokasi & nama foto staff
+        avatar: "nainn.jpg", // Gantilah sesuai lokasi & nama foto staff
         roblox: "@9ineverse",
         joined: "AGUSTUS 2026",
         events: "5+ Event",
@@ -190,7 +190,7 @@ window.onclick = function(event) {
       {
         name: "CESHI",
         role: "WAKIL OWNER",
-        avatar: "gambar/staff2.png",
+        avatar: "cess.jpg",
         roblox: "@alexiciusbro",
         joined: "AGUSTUS 2023",
         events: "3 Event",
